5da7cef8f055f6bd8bd5bbdcecabaec46064853a
Baron_2333 <2391847636@qq.com>
2026-05-18 23:57:15 +0800

    清理拼音映射表重复键\

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,6 +1,6 @@
 // 版本信息 - 每次发布时更新
-export const VERSION = 'v0.1.32';
-export const BUILD_TIME = '2026-05-18 23:56';
+export const VERSION = 'v0.1.33';
+export const BUILD_TIME = '2026-05-18 23:57';
 
 // 获取格式化的时间
 export function getFormattedBuildTime(): string {
